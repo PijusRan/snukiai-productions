43fa9e55b7a034cc8e5b3b6d294a672c85856457
Pijus Ran <pijusranonis@gmail.com>
2024-09-14 17:56:17 +0300

    Fixed Game Page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,13 +10,10 @@ export default function homePage(){
             <div className="AboutSection">
             <img src={Logo} className="Logo"/>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Realiai trūputį ligoti esam...
                 </p>
             </div>
             
-            
-
-
             <img src={BG1} className="BG1"/>
         </div>
     )
--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,9 +1,13 @@
 import "react";
+import "./game.css";
+
+import KiskisPetriskis from "../assets/Running.gif";
 
 export default function homePage(){
     return(
-        <div>
-            <h1>Cia turi zaidimas but</h1>
+        <div className="Game">
+            <h1>PIJAU DARYK ZAIDIMA JOBSIKMAT</h1>
+            <img src={KiskisPetriskis}/>
         </div>
     )
 }
