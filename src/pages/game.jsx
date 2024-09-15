dfc0b0c1d4d21780b1156aab098710953f4cc5e6
Pijus Ran <pijusranonis@gmail.com>
2024-09-15 19:45:56 +0300

    Added game

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,13 +1,10 @@
 import "react";
 import "./game.css";
 
-import KiskisPetriskis from "../assets/Running.gif";
-
 export default function GamePage(){
     return(
         <div className="Game">
-            <h1>PIJAU DARYK ZAIDIMA JOBSIKMAT</h1>
-            <img src={KiskisPetriskis}/>
+            <iframe frameborder="0" src="https://itch.io/embed-upload/11485114?color=333333" allowfullscreen="" width="1920" height="1100"></iframe>
         </div>
     )
 }
