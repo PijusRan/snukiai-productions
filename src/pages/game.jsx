cea25d490e77702ea1cfa88f91a17d1ceb3e9c98
Pijus Ran <pijusranonis@gmail.com>
2024-09-16 20:00:54 +0300

    Updated background, game

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -4,7 +4,7 @@ import "./game.css";
 export default function GamePage(){
     return(
         <div className="Game">
-            <iframe frameborder="0" src="https://itch.io/embed-upload/11485114?color=333333" allowfullscreen="" width="1920" height="1100"></iframe>
+            <iframe frameborder="0" src="https://itch.io/embed-upload/11496199?color=333333" allowfullscreen="" width="640" height="380"></iframe>
         </div>
     )
 }
